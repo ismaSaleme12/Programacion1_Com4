1fac069707b4e691434f174935a53319017e63cc
isma saleme <ismasaleme12@gmail.com>
2025-08-20 16:25:25 -0300

    archivos tp.py

---
+++ ejercicio_secuenciales.py
@@ -0,0 +1,24 @@
+print ("CALCULADORA DE PROPINAS")
+
+total_cuenta=input("INGRESA EL MONTO TOTAL DE LA CUENTA: ")
+
+propina1=float(0.10)
+propina2=float(0.15)
+
+print("PROPINA SUGERIDA (10%)")
+
+#total de propina sugerida (10%)
+propina1_total=total_cuenta*propina1
+
+print(propina1_total)
+print("TOTAL A PAGAR")
+print(total_cuenta+propina1_total)
+
+print("PROPINA SUGERIDA (20%)")
+
+#total de propina sugerida (20%)
+propina2_total=total_cuenta*propina2
+
+print(propina2_total)
+print("TOTAL A PAGAR")
+print(total_cuenta+propina2_total)
