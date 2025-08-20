ab236b0f1fbf38105ab73bf4c09e8300e29c13b2
ismaSaleme12 <ismasaleme12@gmail.com>
2025-08-20 16:52:21 -0300

    Add files via upload

---
+++ tp secuenciales_complemenatrios/python.py
@@ -0,0 +1,71 @@
+print("ejercicio 1")
+numero1=int(9)
+print(numero1)
+
+print("ejercicio 2")
+numero2=float(9.2)
+print(numero2)
+
+print("ejercicio 3")
+suma=numero1+numero2
+print(suma)
+
+print("ejercicio 4")
+resta=numero1-numero2
+multi=numero1*numero2
+divis=numero1/numero2
+
+print(resta)
+print(multi)
+print(divis)
+
+print("ejercicio 5")
+nombre="ismael"
+print(nombre)
+
+print("ejercicio 6")
+precio=float(23.5)
+print(precio)
+
+print("ejercicio 7")
+descuento=float(0.40)
+print(descuento)
+
+print("ejercicio 8")
+precio_final= precio*descuento
+print(precio_final)
+
+print("ejercicio 9")
+cadena="hola mundo"
+print(cadena)
+
+print("ejercicio 10")
+longitud=len(cadena)
+print(longitud)
+
+print("ejercicio 11")
+precio=int(89.9)
+print(precio)
+
+print("ejercicio 12")
+nombre="ismael"
+apellido="saleme"
+nombre_completo= nombre + " " + apellido
+print(nombre_completo)
+
+print("ejercicio 13")
+edad=19
+edad+=5
+edad-=10
+print(edad)
+
+print("ejercicio 14")
+altura=float(1.90)
+altura*= 4
+altura/=10
+print(altura)
+
+print("ejercicio 15")
+nombre="ISMAEL"
+nombre=nombre.lower()
+print(nombre)
