70a73057be4f5ac20e3c2198ee4912153aa7a598
isma saleme <ismasaleme12@gmail.com>
2025-10-02 17:37:31 -0300

    PROGRAMACION 1 - COMISION 4

--- 06_Unidad6/TP_funciones.py
+++ 06_Unidad6/TP_funciones.py
@@ -7,7 +7,8 @@
 def saludar():
     return("Hola, mundo!")
 
-#programa principal
+#principal
+print("EJERCICIO 1")
 saludo = saludar()
 print(saludo)
 
@@ -18,7 +19,8 @@ print(saludo)
 def saludar_nombre(nombre):
     return(f"Hola, {nombre}!")
 
-#Programa Principal
+#principal
+print("EJERCICIO 2")
 nombre = input("Ingrese su nombre: ")
 
 print(saludar_nombre(nombre))
@@ -31,6 +33,7 @@ def informacion_personal(nmb,apell,edad,resid):
     return(f"Soy {nmb} {apell}, tengo {edad} años y vivo en {resid}")
 
 #principal
+print("EJERCICIO 3")
 nombre = input("Ingrese su nombre: ")
 apellido = input("Ingrese su apellido: ")
 edad = int(input("Ingrese su edad: "))
@@ -54,6 +57,7 @@ def calcular_perimetro_circulo(radio):
 
 
 #principal
+print("EJERCICIO 4")
 radio = float(input("ingrese el radio del circulo: "))
 
 print(calcular_area_circulo(radio))
@@ -68,6 +72,7 @@ def segundos_a_horas(seg):
     return f"{seg} segundos son {horas} horas"
 
 #principal
+print("EJERCICIO 5")
 segundos = int(input("Ingrese la cantidad de segundos a: "))
 
 print(segundos_a_horas(segundos))
@@ -80,8 +85,9 @@ def tabla_multiplicar(num):
     print(f"TABLA DE MULTIPLICAR DEL {num}")
     for i in range(1,11):
         print(f"{num} x {i} = {num * i}")
-#principal
 
+#principal
+print("EJERCICIO 6")
 numero = int(input("De que numero quiere saber la tabla de multiplicar?: "))
 
 tabla_multiplicar(numero)
@@ -100,6 +106,7 @@ def operaciones_basicas(n1,n2):
     return (suma, resta, multiplicacion, division)
 
 #principal
+print("EJERCICIO 7")
 num1 = int(input("Ingrese el primer numero: "))
 num2 = int(input("Ingrese el segundo numero: "))
 
@@ -112,15 +119,52 @@ print(f"La division es: {resultados[3]}")
     
 
 #-----------------------------------------
-#Ejercicio 7
+#Ejercicio 8
 
 #funcion
 def indice_masa_corporal(peso, altura):
     imc = peso / (altura ** 2)
-    return f"Su indice de masa corporal es: {imc}"
+    return imc
+
 #principal
+print("EJERCICIO 8")
 peso = float(input("Ingrese su peso en kg: "))
 altura = float(input("Ingrese su altura en metros: "))
 
-imc = indice_masa_corporal(peso, altura)
+resultado_imc = indice_masa_corporal(peso, altura)
 
+print(f"Su indice de masa corporal es de: {resultado_imc:2.2f}")
+
+#-----------------------------------------
+#Ejercicio 9
+
+#funcion
+def celsius_a_fahrenheit(cel):
+    fahrenheit = (cel * 9/5) + 32
+    return fahrenheit
+
+#principal
+print("EJERCICIO 9")
+celsius = float(input("Ingrese la temperatura en grados Celsius: "))
+
+resultado = celsius_a_fahrenheit(celsius)
+
+print(f"{celsius} grados Celsius son {resultado} grados Fahrenheit")
+
+#-----------------------------------------
+#Ejercicio 10
+
+#funcion
+def calcular_promedio(n1,n2,n3):
+    promedio = (n1 + n2 + n3) / 3
+    return promedio
+
+#principal
+print("EJERCICIO 10")
+num1 = int(input("Ingrese el primer numero: "))
+num2 = int(input("Ingrese el segundo numero: "))
+num3 = int(input("Ingrese el tercer numero: "))
+
+resultado = calcular_promedio(num1, num2, num3)
+
+print(f"El promedio de los tres numeros es: {resultado:2.2f}")
